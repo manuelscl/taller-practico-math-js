9e07bc16c59ac9459a99fc5cfe883538a7402fbd
manuelscl <manuelscardenaslopez@gmail.com>
2023-01-12 10:08:20 -0600

    Calculando la circunferencia y área de un círculo

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Cuadrado
 console.group('CUADRADO');
 function calcularCuadrado(lado) {
     return {
@@ -8,7 +9,9 @@ function calcularCuadrado(lado) {
 console.log(calcularCuadrado(5));
 console.groupEnd('CUADRADO');
 
+// Triángulo
 console.group('TRIANGULO');
+
 function calcularTriangulo(lado1, lado2, base, altura) {
     return {
         perimetro: lado1 + lado2 + base,
@@ -16,4 +19,21 @@ function calcularTriangulo(lado1, lado2, base, altura) {
     }
 }
 console.log(calcularTriangulo(6,6,4,5.5));
+
 console.groupEnd('TRIANGULO');
+
+// Círculo
+console.group('CIRCULO');
+
+function calcularCirculo(radio) {
+    const diametro = radio * 2;
+    const radioAlCuadrado = Math.pow(radio, 2);
+
+    return {
+        circunferencia: diametro * Math.PI,
+        area: radioAlCuadrado * Math.PI,
+    }
+}
+console.log(calcularCirculo(20));
+
+console.groupEnd('CIRCULO');
