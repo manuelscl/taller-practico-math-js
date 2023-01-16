622d4f5ffb9066bd60a0bc250a08d2888e013ea0
manuelscl <manuelscardenaslopez@gmail.com>
2023-01-16 08:29:58 -0600

    Calculando la moda, elemento que más se repite

--- moda.js
+++ moda.js
@@ -10,5 +10,21 @@ function calcularModa(lista) {
             listaCount[elemento] = 1;
         }
     }
-    console.log(listaCount);
+    const listaArray = Object.entries(listaCount);
+    const listaOrdenada = ordenarListaBidimensional(listaArray, 1);
+    const ultimaPosicion = listaOrdenada[listaOrdenada.length - 1];
+    // console.log({listaCount, listaArray, listaOrdenada, ultimaPosicion});
+    // console.log('La moda es: ' + ultimaPosicion[0]);
+    const moda = ultimaPosicion[0];
+
+    return moda;
+}
+
+function ordenarListaBidimensional(listaDesordenada, i) {
+    function ordenarLista(valorAcumulado, valorActual) {
+        return valorAcumulado[i] - valorActual[i];
+    }
+
+    const lista = listaDesordenada.sort(ordenarLista);
+    return lista;
 }
