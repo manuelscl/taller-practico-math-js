11408ea21c78e0a3208d65bc7c83b03d1f350497
manuelscl <manuelscardenaslopez@gmail.com>
2023-01-21 18:59:27 -0600

    Calculando la mediana de los salarios de una empresa en cada año

--- analisis.js
+++ analisis.js
@@ -52,7 +52,6 @@ const empresas = {};
 
 let company;
 for(persona of salarios) {
-    // console.log(persona)
     for(trabajo of persona.trabajos) {
         company = trabajo.empresa;
         if(!empresas[trabajo.empresa]) {
@@ -63,5 +62,42 @@ for(persona of salarios) {
             empresas[trabajo.empresa][trabajo.year] = [];
         }
         empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+    }
+}
+
+function medianaEmpresaYear(nombre, year) {
+    if(!empresas[nombre]) {
+        console.warn('La empresa no existe');
+    } else if(!empresas[nombre][year]) {
+        console.warn('La empresa no dió salarios ese año');
+    } else {
+        return PlatziMath.calcularMediana(empresas[nombre][year]);
+    }
+}
+
+function proyeccionPorEmpresa(nombre) {
+    if(!empresas[nombre]) {
+        console.warn('La empresa no existe');
+    } else {
+        const empresaYears = Object.keys(empresas[nombre]);
+        const listaMedianaYears = empresaYears.map((year) => {
+            return medianaEmpresaYear(nombre, year);
+        });
+        console.log({listaMedianaYears});
+
+        let porcentajesCrecimiento = [];
+
+        for(let i = 1; i < listaMedianaYears.length; i++) {
+            const salarioActual = listaMedianaYears[i];
+            const salarioPasado = listaMedianaYears[i - 1];
+            const crecimiento = salarioActual - salarioPasado;
+            const porcentajeCrecimiento = crecimiento / salarioPasado;
+            porcentajesCrecimiento.push(porcentajeCrecimiento);
+        }
+
+        const medianaPorcentajesCrecimiento = PlatziMath.calcularMediana(porcentajesCrecimiento);
+        const ultimaMediana = listaMedianaYears[listaMedianaYears.length - 1];
+        const aumentoDeSalario = ultimaMediana * medianaPorcentajesCrecimiento;
+        const nuevoSalario = ultimaMediana + aumentoDeSalario;
     }
 }
